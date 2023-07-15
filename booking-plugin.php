e07b8aa930a1b49b31b60839f4d8d86eaaec054f
Jason <jasonhan951@gmail.com>
2023-07-15 20:03:24 +0800

    update

--- booking-plugin.php
+++ booking-plugin.php
@@ -20,7 +20,8 @@ class BookingPlugin
     public function __construct()
     {
         define('PLUGIN_PATH', plugin_dir_path(__FILE__));
-        add_action('init', array($this, 'custom_post_type'));
+        define('PLUGIN_URL', plugin_dir_url( __FILE__ ));
+        require_once(PLUGIN_PATH . '/vendor/autoload.php');
     }
 
     public function initialize()
@@ -31,7 +32,7 @@ class BookingPlugin
 
 }
 
-if (!class_exists('BookingPlugin')) {
+if (class_exists('BookingPlugin')) {
     $bookingPlugin = new BookingPlugin();
     $bookingPlugin->initialize();
 }
--- includes/option-page.php
+++ includes/option-page.php
@@ -8,15 +8,26 @@ add_action('carbon_fields_register_fields', 'create_options_page');
 
 function load_carbon_fields()
 {
-      Carbon_Fields\Carbon_Fields::boot();
+      \Carbon_Fields\Carbon_Fields::boot();
 }
 
-function create_options_page() 
+function create_options_page()
 {
-    Container::make( 'theme_options', __( 'Theme Options' ) )
-        ->add_fields( array(
-            Field::make( 'text', 'crb_text', 'Text Field' ),
-        ) );
+    Container::make('theme_options', __('Contact Form'))
+
+    ->set_page_menu_position(1)
+
+    ->set_icon('dashicons-media-text')
+
+    ->add_fields(array(
+
+          Field::make('checkbox', 'contact_plugin_active', __('Active')),
+
+          Field::make('text', 'contact_plugin_recipients', __('Recipient Email'))->set_attribute('placeholder', 'eg. your@email.com')->set_help_text('The email that the form is submitted to'),
+
+          Field::make('textarea', 'contact_plugin_message', __('Confirmation Message'))->set_attribute('placeholder', 'Enter confirmation message')->set_help_text('Type the message you want the submitted to receive'),
+
+    ));
 }
 
 ?>
