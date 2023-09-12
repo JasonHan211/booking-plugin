6cda351efca57ca223b50efbf907c5324c7ea2a8
Jason <jasonhan951@gmail.com>
2023-09-13 00:09:16 +0800

    Update edit-menu.php

--- includes/booking/admin/edit-menu.php
+++ includes/booking/admin/edit-menu.php
@@ -9,7 +9,9 @@ function booking_edit_page() {
 
     if (isset($_GET['action']) && $_GET['action'] === 'edit' && isset($_GET['booking_id'])) {
         $booking_id = intval($_GET['booking_id']);
-        
+        $booking_header = $bookingClass->get_booking_header($booking_id);
+        $bookings = $bookingClass->get_bookings($booking_id);
+        $booking_addons = $bookingClass->get_booking_addons($booking_id);
     }
 
     if (isset($_POST['update_booking'])) {
@@ -39,30 +41,30 @@ function booking_edit_page() {
         <h1>Edit booking</h1>
         <form method="post" action="">
             <label for="booking_date_from">Date From:</label>
-            <input type="date" name="booking_date_from" value="<?php echo esc_attr($booking['booking_date_from']); ?>" required>
+            <input type="date" name="booking_date_from" value="<?php echo esc_attr($booking_header['booking_date_from']); ?>" required>
             <label for="booking_date_to">Date To:</label>
-            <input type="date" name="booking_date_to" value="<?php echo esc_attr($booking['booking_date_to']); ?>" required>
+            <input type="date" name="booking_date_to" value="<?php echo esc_attr($booking_header['booking_date_to']); ?>" required>
             <label for="booking_resource">Resource:</label>
-            <input type="text" name="booking_resource" value="<?php echo esc_attr($booking['booking_resource']); ?>" required>
+            <input type="text" name="booking_resource" value="<?php echo esc_attr($booking_header['booking_resource']); ?>" required>
             <label for="booking_notes">Notes:</label>
-            <textarea name="booking_notes"><?php echo esc_textarea($booking['booking_notes']); ?></textarea>
+            <textarea name="booking_notes"><?php echo esc_textarea($booking_header['booking_notes']); ?></textarea>
             <label for="booking_details">Description:</label>
-            <textarea name="booking_description"><?php echo esc_textarea($booking['booking_description']); ?></textarea>
+            <textarea name="booking_description"><?php echo esc_textarea($booking_header['booking_description']); ?></textarea>
             <label for="booking_paid">Paid:</label>
             <select name="booking_paid">
-                <option value="Y" <?php if ($booking['booking_paid'] === 'Y') echo 'selected'; ?>>YES</option>
-                <option value="N" <?php if ($booking['booking_paid'] === 'N') echo 'selected'; ?>>NO</option>
+                <option value="Y" <?php if ($booking_header['booking_paid'] === 'Y') echo 'selected'; ?>>YES</option>
+                <option value="N" <?php if ($booking_header['booking_paid'] === 'N') echo 'selected'; ?>>NO</option>
             </select>
             <label for="booking_adults">Adults:</label>
-            <input type="number" name="booking_adults" value="<?php echo esc_attr($booking['booking_adults']); ?>" required>
+            <input type="number" name="booking_adults" value="<?php echo esc_attr($booking_header['booking_adults']); ?>" required>
             <label for="booking_children">Children:</label>
-            <input type="number" name="booking_children" value="<?php echo esc_attr($booking['booking_children']); ?>" required>
+            <input type="number" name="booking_children" value="<?php echo esc_attr($booking_header['booking_children']); ?>" required>
             <label for="booking_user">Name:</label>
-            <input type="text" name="booking_user" value="<?php echo esc_attr($booking['booking_user']); ?>" required>
+            <input type="text" name="booking_user" value="<?php echo esc_attr($booking_header['booking_user']); ?>" required>
             <label for="booking_email">Email:</label>
-            <input type="text" name="booking_email" value="<?php echo esc_attr($booking['booking_email']); ?>" required>
+            <input type="text" name="booking_email" value="<?php echo esc_attr($booking_header['booking_email']); ?>" required>
             <label for="booking_phone">Phone:</label>
-            <input type="text" name="booking_phone" value="<?php echo esc_attr($booking['booking_phone']); ?>" required>
+            <input type="text" name="booking_phone" value="<?php echo esc_attr($booking_header['booking_phone']); ?>" required>
             <input type="submit" name="update_booking" value="Update booking">
         </form>
     </div>
